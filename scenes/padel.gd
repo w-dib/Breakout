e7941002437cdd7156d2776109b4939bf4f9c57d
Walid Daniel Dib <112762655+w-dib@users.noreply.github.com>
2023-11-24 14:02:13 +0400

    Add movement

---
+++ scenes/padel.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+@export var SPEED = 500
+func _process(_delta):
+
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
