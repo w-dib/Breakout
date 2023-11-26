867d7902724395e3f24aee65cb0fc18ebd7f563c
Walid Daniel Dib <112762655+w-dib@users.noreply.github.com>
2023-11-26 22:33:12 +0400

    Add score and game over button

--- scenes/ball.gd
+++ scenes/ball.gd
@@ -1,15 +1,20 @@
 extends CharacterBody2D
 
-@export var initial_ball_speed = 450
+@onready var score = $"../GUI/Label"
+@onready var gameover = $"../GUI/Gameover"
+
+
+@export var initial_ball_speed = 300
 @export var speed_multiplier = 1.02
 
 var ball_speed = initial_ball_speed
-
+var score_value = 0
 var direction: Vector2 = Vector2.ZERO
 
 func _ready():
 	start_ball()
-
+	gameover.hide()
+	
 func start_ball():
 	randomize()
 	direction = Vector2(randf_range(-1,1), randf_range(0.5,1)).normalized()
@@ -22,3 +27,6 @@ func _physics_process(delta):
 		if collision.get_collider().is_in_group("Bricks"):
 			var brick = collision.get_collider()
 			brick.queue_free()
+			score_value += 1
+			print(score_value)
+			score.text = "SCORE: %s" % score_value
--- scenes/main.gd
+++ scenes/main.gd
@@ -1,1 +1,21 @@
 extends Node
+
+@onready var gameover = $GUI/Gameover
+@onready var padel = $Padel
+@onready var restart = $GUI/Restart
+
+func _on_lower_wall_body_entered(_body):
+	gameover.show()
+	padel.hide()
+	restart.show()
+	hide_all_bricks()
+	
+func hide_all_bricks():
+	var bricks = get_tree().get_nodes_in_group("Bricks")
+	for brick in bricks:
+		brick.hide()
+	
+
+
+func _on_restart_pressed():
+	get_tree().reload_current_scene()
