296470e719e3a4e6eb1424b4b059b646fee01f9c
Walid Daniel Dib <112762655+w-dib@users.noreply.github.com>
2023-11-26 16:17:28 +0400

    Fix velocity

--- scenes/ball.gd
+++ scenes/ball.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@export var initial_ball_speed = 20
+@export var initial_ball_speed = 450
 @export var speed_multiplier = 1.02
 
 var ball_speed = initial_ball_speed
@@ -10,14 +10,12 @@ var direction: Vector2 = Vector2.ZERO
 func _ready():
 	start_ball()
 
-
 func start_ball():
 	randomize()
 	direction = Vector2(randf_range(-1,1), randf_range(0.5,1)).normalized()
 	velocity = direction * ball_speed  
 	
 func _physics_process(delta):
-	var collision = move_and_collide(velocity * ball_speed * delta)
-	
+	var collision = move_and_collide(velocity * delta)
 	if collision:
 		velocity = velocity.bounce(collision.get_normal()) * speed_multiplier 
