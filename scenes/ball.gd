f1757a734b354380f58347399fdfff51b99ebc48
Walid Daniel Dib <112762655+w-dib@users.noreply.github.com>
2023-11-26 12:59:12 +0400

    Add bounce to ball

---
+++ scenes/ball.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+@export var initial_ball_speed = 20
+@export var speed_multiplier = 1.02
+
+var ball_speed = initial_ball_speed
+
+var direction: Vector2 = Vector2.ZERO
+
+func _ready():
+	start_ball()
+
+
+func start_ball():
+	randomize()
+	direction = Vector2(randf_range(-1,1), randf_range(0.5,1)).normalized()
+	velocity = direction * ball_speed  
+	
+func _physics_process(delta):
+	var collision = move_and_collide(velocity * ball_speed * delta)
+	
+	if collision:
+		velocity = velocity.bounce(collision.get_normal()) * speed_multiplier 
--- scenes/padel.gd
+++ scenes/padel.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 @export var SPEED = 500
 func _process(_delta):
-
+	velocity.y = 0
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
